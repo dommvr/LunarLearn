3f52515aaafd2997dd3631fe55270c1f13aa398a
dommvr <dommar0028@gmail.com>
2025-10-24 17:56:14 +0200

    Added RMSNorm layer

--- LunarLearn/layers/RMSNorm.py
+++ LunarLearn/layers/RMSNorm.py
@@ -7,18 +7,16 @@ from LunarLearn.tensor import ops
 xp = backend.xp
 DTYPE = backend.DTYPE
 
-class LayerNorm(BaseLayer):
+class RMSNorm(BaseLayer):
     def __init__(self, normalize_shape, epsilon=1e-5):
         super().__init__(trainable=True)
-        self.normalize_shape = normalize_shape # e.g. (C,) or (C, H, W)s
+        self.normalize_shape = normalize_shape # e.g. (C,) or (C, H, W)
         self.epsilon = xp.array(epsilon, dtype=DTYPE)
 
     def initialize(self, input_shape):
         W = xp.ones(self.normalize_shape, dtype=DTYPE)
-        b = xp.zeros(self.normalize_shape, dtype=DTYPE)
 
         self.W = Parameter(W, requires_grad=True)
-        self.b = Parameter(b, requires_grad=True)
         self.output_shape = input_shape
 
     def forward(self, Z: Tensor) -> Tensor:
@@ -26,14 +24,11 @@ class LayerNorm(BaseLayer):
             self.initialize(Z.shape[1:])
 
         W = self.W.to_compute()
-        b = self.b.to_compute()
 
-        axes = tuple(range(1, len(Z.shape)))
-        mean = ops.mean(Z, axis=axes, keepdims=True)
-        var = ops.var(Z, axis=axes, keepdims=True)
-        denom = ops.sqrt(var + self.epsilon)
-        inv_denom = 1.0 / denom
-        Z_norm = (Z - mean) * inv_denom
-        Z_out = W * Z_norm + b
+        axes = tuple(range(1, Z.ndim))
+        rms = ops.sqrt(ops.mean(Z * Z, axis=axes, keepdims=True))
+        inv_denom = 1.0 / rms
+        Z_norm = Z * inv_denom
+        Z_out = W * Z_norm
 
         return Z_out
