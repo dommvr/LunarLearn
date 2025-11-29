af3240a6c8ff750aab4518722e7b2e10f89be89e
Dominik <dommar0028@gmail.com>
2025-11-29 22:31:47 +0100

    Added DeepLab models

--- LunarLearn/train/models/segmentation/__init__.py
+++ LunarLearn/train/models/segmentation/__init__.py
@@ -1,5 +1,9 @@
+from .deeplab import DeepLabV3
+from .deeplab import DeepLabV3Plus
 from .unet import UNet
 
 __all__ = [
+    "DeepLabV3",
+    "DeepLabV3Plus",
     "UNet"
 ]
--- LunarLearn/train/models/segmentation/deeplab.py
+++ LunarLearn/train/models/segmentation/deeplab.py
@@ -64,7 +64,8 @@ class ASPP(BaseLayer):
         x = self.project_norm(x)
         x = self.project_act(x)
         return x
-    
+
+
 class DeepLabV3(Module):
     def __init__(self,
                  num_classes,
@@ -116,5 +117,117 @@ class DeepLabV3(Module):
         x = ops.upsample(x, scale_factor)
         out = self.final_act(x)
         return out
+    
 
+class DeepLabV3Plus(Module):
+    def __init__(self,
+                 num_classes,
+                 filters=64,
+                 depth=4,
+                 atrous_rates=(6, 12, 18),
+                 norm_layer=BatchNorm2D,
+                 activation="relu",
+                 final_activation=None,
+                 low_level_filters=48,
+                 pretrained=False):
+        from LunarLearn.train.models.segmentation.unet import DownBlock, DoubleConv
+        from LunarLearn.nn.activations import get_activation
+        super().__init__()
+
+        self.num_classes = num_classes
+        self.depth = depth
+
+        self.encoder = ModuleList([
+            DownBlock(filters * (2 ** i), norm_layer=norm_layer, activation=activation)
+            for i in range(depth)
+        ])
+
+        bottleneck_filters = filters * (2 ** depth)
+        self.bottleneck = DoubleConv(bottleneck_filters,
+                                     norm_layer=norm_layer,
+                                     activation=activation)
         
+        self.aspp = ASPP(filters=bottleneck_filters,
+                         atrous_rates=atrous_rates,
+                         norm_layer=norm_layer,
+                         activation=activation)
+        
+        self.low_level_conv = Conv2D(filters=low_level_filters,
+                                     kernel_size=1,
+                                     strides=1)
+        self.low_level_norm = norm_layer()
+        self.low_level_act = get_activation(activation)
+
+        self.decoder_conv1 = Conv2D(filters=filters,
+                                    kernel_size=3,
+                                    strides=1,
+                                    padding=1)
+        self.decoder_norm1 = norm_layer()
+        self.decoder_act1 = get_activation(activation)
+
+        self.decoder_conv2 = Conv2D(filters=filters,
+                                    kernel_size=3,
+                                    strides=1,
+                                    padding=1)
+        self.decoder_norm2 = norm_layer()
+        self.decoder_act2 = get_activation(activation)
+
+        # Final classifier (on decoder output)
+        self.final_conv = Conv2D(filters=num_classes,
+                                 kernel_size=1,
+                                 strides=1)
+        
+        self.final_act = get_activation(final_activation)
+
+        if pretrained:
+            self.load_state_dict(None)
+
+    def forward(self, x: Tensor):
+        input_h, input_w = x.shape[2], x.shape[3]
+
+        # ----- Encoder -----
+        low_level = None
+        for idx, block in enumerate(self.encoder):
+            x, skip = block(x)  # DownBlock returns (downsampled, skip)
+            if idx == 0:
+                # use first high-res skip as low-level feature
+                low_level = skip
+
+        # ----- Bottleneck + ASPP -----
+        x = self.bottleneck(x)
+        x = self.aspp(x)  # shape: (B, bottleneck_filters, H_aspp, W_aspp)
+
+        # ----- Upsample ASPP to low-level spatial size -----
+        low_h, low_w = low_level.shape[2], low_level.shape[3]
+        feat_h, feat_w = x.shape[2], x.shape[3]
+        scale_h = low_h // feat_h
+        scale_w = low_w // feat_w
+        # assuming OS is consistent so scale_h == scale_w
+        x = ops.upsample(x, scale_factor=scale_h, mode="bilinear")
+
+        # ----- Project low-level feature -----
+        low = self.low_level_conv(low_level)
+        low = self.low_level_norm(low)
+        low = self.low_level_act(low)
+
+        # ----- Concatenate & decoder -----
+        x = ops.concatenate([x, low], axis=1)
+
+        x = self.decoder_conv1(x)
+        x = self.decoder_norm1(x)
+        x = self.decoder_act1(x)
+
+        x = self.decoder_conv2(x)
+        x = self.decoder_norm2(x)
+        x = self.decoder_act2(x)
+
+        # ----- Final classifier & upsample to input size -----
+        x = self.final_conv(x)
+
+        feat_h, feat_w = x.shape[2], x.shape[3]
+        scale_h = input_h // feat_h
+        scale_w = input_w // feat_w
+        x = ops.upsample(x, scale_factor=scale_h, mode="bilinear")
+
+        out = self.final_act(x)
+        return out
